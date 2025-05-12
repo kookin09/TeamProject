6768d693b906c4f017fbde003c44c97816a50206
jellujoa <jellujoa03@gmail.com>
2025-05-12 14:56:57 +0900

    1

--- TeamProject/Assets/Kookin/Script/ColliderObject.cs
+++ TeamProject/Assets/Kookin/Script/ColliderObject.cs
@@ -4,37 +4,40 @@ using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
-public class ColliderObject : MonoBehaviour
+namespace Assets.Kookin.Script
 {
-    HpBar hpBar; //HpBar 스크립트의 객체를 담을 변수
-    GameManager gameManager ; //GameManager 스크립트의 객체를 담을 변수
-    private void Start()
+    public class ColliderObject : MonoBehaviour
     {
-        hpBar = GameObject.Find("HpBar").GetComponent<HpBar>(); //HpBar 스크립트의 객체를 찾는다
-        
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-       
-        if (collision.gameObject.tag == "") //""태그의 물체와 충돌하였을 경우
+        HpBar hpBar; //HpBar 스크립트의 객체를 담을 변수
+        GameManager gameManager; //GameManager 스크립트의 객체를 담을 변수
+        private void Start()
         {
-            if(hpBar.currentHp >= 0) //체력이 0보다 클 때
-            {
-                hpBar.UpdateHp(10); //hp 10감소
-            }
-            else
-            {
-                 gameManager.GameOver(); //반대의 경우 게임종료
-            }
+            hpBar = GameObject.Find("HpBar").GetComponent<HpBar>(); //HpBar 스크립트의 객체를 찾는다
+
         }
-        else if (collision.gameObject.tag == "") //""태그의 물체와 충돌하였을 때
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            hpBar.currentHp += 10; //hp 10회복
-            
-        }
-        else if (collision.gameObject.tag == "")
-        {
-           //Score += 1000;
+
+            if (collision.gameObject.tag == "") //""태그의 물체와 충돌하였을 경우
+            {
+                if (hpBar.currentHp >= 0) //체력이 0보다 클 때
+                {
+                    hpBar.UpdateHp(10); //hp 10감소
+                }
+                else
+                {
+                    gameManager.GameOver(); //반대의 경우 게임종료
+                }
+            }
+            else if (collision.gameObject.tag == "") //""태그의 물체와 충돌하였을 때
+            {
+                hpBar.currentHp += 10; //hp 10회복
+
+            }
+            else if (collision.gameObject.tag == "")
+            {
+                //Score += 1000;
+            }
         }
     }
 }
